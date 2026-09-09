46608f48ef31847a0ef7db19c85f05ed26ad328c
Jay <jusgav-1@student.ltu.se>
2026-09-09 14:13:14 +0200

    docs: added additional documentation to network.go file

--- kademlia/network.go
+++ kademlia/network.go
@@ -11,21 +11,42 @@ import (
 	"time"
 )
 
+// Network handles low-level UDP-socket communication and incoming RPC dispatching 
 type Network struct {
-	conn     *net.UDPConn // net: package for portable interface for network I/O
+
+	// conn is the active UDP socket listener. 
+	conn     *net.UDPConn 
+	
+	// kademlia points to the local node to allow the network 
+	// layer to update the routing table etc.
 	kademlia *Kademlia
+
+	//mu is a mutex lock.
 	mu       sync.Mutex
+
+	// RPC maps in-transmission Transaction IDs to their response channels.
 	RPC      map[string]chan RPCMessage
 }
 
+// RPCMessage contains all transmission-critical information
 type RPCMessage struct {
-	Type          string      `json:"type"` // PING, PONG, etc.
+	// PING, PONG, etc.
+	Type          string      `json:"type"`
+
+	// Unique ID to distinguish between in-transmission messages 
 	TransactionID string      `json:"tx_id"`
+
+	// Sender of the message 
 	Sender        Contact     `json:"sender"`
+
+	// Target of the message
 	Target        *KademliaID `json:"target, omitempty"`
+
+	// A contact list 
 	Contacts      []Contact   `json:"contacts, omitempty"`
 }
 
+// NewNetwork to create a new network instance 
 func NewNetwork(kademlia *Kademlia) *Network {
 	return &Network{
 		kademlia: kademlia,
@@ -33,6 +54,8 @@ func NewNetwork(kademlia *Kademlia) *Network {
 	}
 }
 
+// Listen binds a UDP socket to the specified IP and port, starting a background 
+// listener loop to recieve and process incoming network packets.
 func (network *Network) Listen(ip string, port int) error {
 
 	// Set up UDP ip:port
@@ -88,6 +111,8 @@ func (network *Network) Listen(ip string, port int) error {
 	return nil
 }
 
+// handleRequest processes incoming RPC requests, updates the routing table 
+// with the sender's contact info, and dispatches the appropriate reply.
 func (network *Network) handleRequest(msg RPCMessage, from *net.UDPAddr) {
 	// Update routing table with senders info
 	if network.kademlia != nil && msg.Sender.ID != nil {
@@ -112,45 +137,55 @@ func (network *Network) handleRequest(msg RPCMessage, from *net.UDPAddr) {
 	}
 }
 
+// SendPingMessage transmits a PING RPC to the target contact and blocks until 
+// a corresponding PONG is recieved or the request times out.
 func (network *Network) SendPingMessage(contact *Contact) (*RPCMessage, error) {
+	
 	// generate TransactionID
 	b := make([]byte, 16)
 	_, _ = rand.Read(b)
 	txID := hex.EncodeToString(b)
-
+	
+	// Channel and link transaction ID
 	respChan := make(chan RPCMessage, 1)
 
 	network.mu.Lock()
 	network.RPC[txID] = respChan
 	network.mu.Unlock()
 
+	// delete when done
 	defer func() {
 		network.mu.Lock()
 		delete(network.RPC, txID)
 		network.mu.Unlock()
 	}()
 
+	// create PING RPC message
 	msg := RPCMessage{
 		Type:          "PING",
 		TransactionID: txID,
 		Sender:        network.kademlia.Me,
 	}
 
+	// get target/remote address
 	rAddr, err := net.ResolveUDPAddr("udp", contact.Address)
 	if err != nil {
 		return nil, err
 	}
 
+	// marshal the outgoing RPC PING
 	data, err := json.Marshal(msg)
 	if err != nil {
 		return nil, err
 	}
 
+	// write message to channel 
 	_, err = network.conn.WriteToUDP(data, rAddr)
 	if err != nil {
 		return nil, err
 	}
 
+	// either response or time out
 	select {
 	case resp := <-respChan:
 		return &resp, nil
