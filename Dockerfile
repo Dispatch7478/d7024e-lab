341be5d9aca229513f0ff1d94adbb00ff8539ace
Dispatch <nahtse-2@student.ltu.se>
2026-09-09 16:29:30 +0200

    docker setup for the ping from yesterday's commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
+FROM golang:1.26.2-alpine AS builder
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -o kadlab main.go
+
 FROM alpine:latest
+WORKDIR /root/
+COPY --from=builder /app/kadlab .
+CMD ["./kadlab"]
+
 
 # Add the commands needed to put your compiled go binary in the container and
 # run it when the container starts.
--- main.go
+++ main.go
@@ -5,13 +5,63 @@ package main
 import (
 	"d7024e/kademlia"
 	"fmt"
+	"log/slog"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	fmt.Println("Pretending to run the kademlia app...")
-	// Using stuff from the kademlia package here. Something like...
-	id := kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	contact := kademlia.NewContact(id, "localhost:8000")
-	fmt.Println(contact.String())
-	fmt.Printf("%v\n", contact)
+	portStr := getEnv("PORT", "8000")
+	port, _ := strconv.Atoi(portStr)
+	ip := getEnv("IP", "0.0.0.0")
+	advertisedIP := getEnv("ADVERTISED_IP", "127.0.0.1")
+
+	listenAddr := fmt.Sprintf("%s:%d", advertisedIP, port)
+
+	id := kademlia.NewKademliaIDFromAddress(listenAddr)
+	me := kademlia.NewContact(id, listenAddr)
+
+	slog.Info("Starting node", "address", listenAddr, "id", id.String())
+
+	rt := kademlia.NewRoutingTable(me)
+	network := kademlia.NewUDPNewtork(me, rt)
+	if err := network.Listen(ip, port); err != nil {
+		slog.Error("failed to listen", "err", err)
+		os.Exit(1)
+	}
+
+	pingTarget := os.Getenv("PING_TARGET")
+	if pingTarget != "" {
+		go func() {
+			time.Sleep(2 * time.Second) // Wait for target node to start
+			slog.Info("Pinging target...", "target", pingTarget)
+
+			targetID := kademlia.NewKademliaIDFromAddress(pingTarget)
+
+			targetContact := kademlia.NewContact(targetID, pingTarget)
+
+			start := time.Now()
+
+			res, err := network.SendPingMessage(&targetContact)
+
+			if err != nil {
+				slog.Error("Ping failed", "err", err)
+				return
+			}
+
+			rtt := time.Since(start)
+
+			slog.Info("Ping worked", "from", res.Sender.Address, "rtt", rtt, "txID", res.TransactionID)
+		}()
+	}
+
+	select {} // Block to keep container up
+}
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
 }
